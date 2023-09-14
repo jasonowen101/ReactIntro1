b6e0ee653e32e234dd49651c47bd3931b6532c76
GHager1776 <GHager1776@gmail.com>
2023-09-14 13:47:38 -0500

    Updated

--- src/GavinHager.js
+++ src/GavinHager.js
@@ -7,18 +7,20 @@ function GavinHager(){
         {id: 103, projectType: "Repair", projectLength: 150, estCostLabor: 8000, bidPrice: 6000, accepted: false},
         {id: 104, projectType: "Construction", projectLength: 400, estCostLabor: 20000, bidPrice: 30000, accepted: true}
 
-        /*{id: 101},
-        {projectType: "Construction"},
-        {projectLength: 300},
-        {estCostLabor: 12000},
-        {bidPrice: 15000},
-        {accepted: true}*/
     ];
 
     return(
         <div>
-            <ProjectList projects={dataList} />
-        </div>
+      <h2>Bogus Project List</h2>
+      <ul>
+        {dataList.map((project) => (
+          <li key={project.id}>
+          {"ID: " + project.id}, {"Type: " + project.projectType}, 
+          {"Length: " + project.projectLength}, {"Est. Cost: " + project.estCostLabor}, 
+          {"Bid Price: " + project.bidPrice}, {"Accepted? " + project.accepted}</li>
+        ))}
+      </ul>
+    </div>
     );
 }
 
