1c80a42ffa02e8f0e4df0fb3d509e29f98f285fe
Chichian2 <tloomis@murraystate.edu>
2023-09-14 13:22:45 -0500

    no idea if this works

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Header from './Header';
 import ProjectList from './ProjectList';
+import TristianLoomis from './TristianLoomis';
 
 function App() {
   const projects = [
@@ -11,11 +12,17 @@ function App() {
     { id: 4, name: 'Project D' },
     { id: 5, name: 'Project E' }
   ];
+  const exampleStuffs = [
+    { id: 1, projectType: 'New Flooring', projectLength: '2 days', estCostLabor: '$600', bidPrice: '$800', accepted: 'No'},
+    { id: 2, projectType: 'New Roofing', projectLength: '3 days', estCostLabor: '$800', bidPrice: '$1200', accepted: 'Yes'},
+    { id: 3, projectType: 'New Windows', projectLength: '10 days', estCostLabor: '$1500', bidPrice: '$2000', accepted: 'No'}
+  ]
 
   return (
     <div>
       <Header />
       <ProjectList projects={projects} />
+      <TristianLoomis exampleStuffs={exampleStuffs} />
     </div>
   );
 }
