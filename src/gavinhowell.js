488e551512935cbf166ee91a4d6d0fb89f594dd2
Gavin596h <ghowell2@murraystate.edu>
2023-09-14 13:42:27 -0500

    added file
    added gavinhowell.js file then added that to app.js file

---
+++ src/gavinhowell.js
@@ -0,0 +1,13 @@
+// gavinhowell.js
+import React from 'react';
+
+var fakeDataObject = {
+    id: 141,
+    projectType: "Big",
+    projectLength: 47,
+    estCostLabor: 200000,
+    bidPrice: 350000,
+    accepted: true
+};
+
+export default fakeDataObject;
